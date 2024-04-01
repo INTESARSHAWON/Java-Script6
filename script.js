c1c88457b01f1cca2352800593a700c05ca0399c
INTESARSHAWON <intesarshawon@gmail.com>
2024-04-01 21:24:37 +0600

    using return

--- script.js
+++ script.js
@@ -11,12 +11,28 @@
 
 
 
-// passing peramiter in function
+// // passing peramiter in function
 
-function myFunction (msg, n) {
-    console.log (msg, n);
+// function myFunction (msg, n) {
+//     console.log (msg, n);
+// }
+
+// myFunction("i love JS", 100); 
+// myFunction("i love Nawshin");
+// // function er moddhe jeta jay oita peramatre, ar function call er moddhe jeta jay oita ke argument bole
+
+
+
+// function 2 number, sum
+function sum (x, y, z) {
+        console.log (x+y+z);
 }
 
-myFunction("i love JS", 100); 
-myFunction("i love Nawshin");
-// function er moddhe jeta jay oita peramatre, ar function call er moddhe jeta jay oita ke argument bole
+// using return
+function sum (x,y) {
+    s=x+y;
+    return s;
+}
+// return diye string, array, object sob return korano jay, kintu ekbar e, eksathe onk kichu na
+// console diye dekhte hobe
+
