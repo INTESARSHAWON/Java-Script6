10c196c9e1eed9edbf889a6785a541d230279b89
INTESARSHAWON <intesarshawon@gmail.com>
2024-04-01 21:21:10 +0600

    passing peramiter in function

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-// Declare and callng function
+// // Declare and callng function
 
-function myFunction () {
-    console.log ("hi");
-    console.log ("i am learning function");
+// function myFunction () {
+//     console.log ("hi");
+//     console.log ("i am learning function");
+// }
+
+// myFunction(); 
+// myFunction();
+
+
+
+
+// passing peramiter in function
+
+function myFunction (msg, n) {
+    console.log (msg, n);
 }
 
-myFunction(); 
-myFunction();
+myFunction("i love JS", 100); 
+myFunction("i love Nawshin");
+// function er moddhe jeta jay oita peramatre, ar function call er moddhe jeta jay oita ke argument bole
